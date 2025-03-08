f9250c7dc10cd03522ee3b33f7da9656a76033ce
Lee Garam <agerio100@naver.com>
2025-03-08 23:51:14 +0900

    Fix: Use gettimeofday() in client to accurately measure time consumption and throughput(X reqs/sec)

--- client/client.c
+++ client/client.c
@@ -3,9 +3,9 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 
-#define NUM_REQUESTS 3000000
+#define NUM_REQUESTS 10 * 1000 * 1000 // 10 million requests
 #define NUM_THREADS 100
 #define REQUESTS_PER_THREAD (NUM_REQUESTS / NUM_THREADS)
 
@@ -18,7 +18,8 @@ int main(int argc, char *argv[]) {
     printf("Client started...\n");
 
     // Log the start time before the pthread loop
-    clock_t start_time = clock();
+    struct timeval start_time, end_time;
+    gettimeofday(&start_time, NULL); // Get the start time
 
     // Spawn threads to send requests
     pthread_t client_threads[NUM_THREADS];
@@ -41,10 +42,13 @@ int main(int argc, char *argv[]) {
     }
 
     // Log the end time after the pthread loop
-    clock_t end_time = clock();
+    gettimeofday(&end_time, NULL); // Get the end time
 
-    // Obtain the statistics
-    double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    // Calculate time taken in seconds
+    double time_taken = (end_time.tv_sec - start_time.tv_sec) +
+                        (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
+
+    // Calculate throughput
     double throughput = NUM_REQUESTS / time_taken;
 
     // Output the time and throughput
